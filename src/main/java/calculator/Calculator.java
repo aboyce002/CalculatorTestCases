5ea41a0c626d0925222667df10f9873f8c1cc746
aboyce002 <aboyce002@my.wilmu.edu>
2019-10-03 13:16:55 -0400

    Update Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,34 +2,34 @@ package calculator;
 
 public class Calculator {
     public int add(int val1, int val2) {
-        return 0;
+        return val1 + val2;
     }
 
     public int subtract(int val1, int val2) {
-        return 0;
+        return val1 - val2;
     }
 
     public double multiply(double val1, double val2) {
-        return 0.0;
+        return val1 * val2;
     }
 
     public double divide(double val1, double val2) {
-        return 0.0;
+        return val1 / val2;
     }
 
     public int square(int value) {
-        return 0;
+        return value * value;
     }
 
     public double squareRoot(double value) {
-        return 0.0;
+        return Math.sqrt(value);
     }
 
     public double exponents(double val1, double val2) {
-        return 0.0;
+        return Math.pow(val1, val2);
     }
 
     public double sineMethod(double radians) {
-        return 0.0;
+        return Math.sin(radians);
     }
 }
